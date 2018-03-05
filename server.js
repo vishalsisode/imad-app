47c6cd3342adea5dcc949b026bfdf294d63de8fb
vishalsisode <36993419+vishalsisode@users.noreply.github.com>
2018-03-05 23:01:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,9 @@ app.get('/:articleName', function (req, res) {
  res.send(createTemplate(articles[articleName]));
 });
 
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

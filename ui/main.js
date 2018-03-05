993904bcb02d7e524651906fba8565bd82bebcf8
vishalsisode <36993419+vishalsisode@users.noreply.github.com>
2018-03-05 23:31:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='new value';
 
 //move the image
-var element=document.getElementById('img');
+var element=document.getElementById('vishal');
 img.onclick= function() {
   img.style.marginLeft= '100px';  
 };
